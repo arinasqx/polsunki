ff2a634d7a1c0b34e46b18d7d01c049ac995e48b
User542Dte <User542Dte@DESKTOP-66UT898>
2024-05-27 00:34:23 +0500

    Добавьте файлы проекта.

---
+++ WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ColorSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            byte red = (byte)RedSlider.Value;
+            byte green = (byte)GreenSlider.Value;
+            byte blue = (byte)BlueSlider.Value;
+
+            Color color = Color.FromRgb(red, green, blue);
+            ColorRectangle.Fill = new SolidColorBrush(color);
+        }
+    }
+}
